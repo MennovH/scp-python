a201b923734a3f73f4561d691d911373c0f5f1de
Menno vH <menno.van.heerde@hva.nl>
2021-01-13 04:02:53 +0100

    scp-python script (renewed)

--- scp-python.py
+++ scp-python.py
@@ -3,185 +3,200 @@ import socket,subprocess,platform,sys,os,getopt
 from colorama import Fore,Back,Style
 from getpass import getpass
 
+red = Fore.RED
+cyan = Fore.CYAN
+green = Fore.GREEN
+yellow = Fore.YELLOW
+reset = Style.RESET_ALL
+
+#get own subnet for easy input
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0].rsplit(".", 1)[0]
 subnet = get_ip()
 
+#check if sshpass is installed
 package = 'sshpass'
 try:
     command = f'which {package}'
     check = subprocess.getoutput(command)
     if not check:
-        print (f'{Fore.RED}sshpass not installed\n{Fore.GREEN}installing...{Style.RESET_ALL}')
+        print (f'{red}sshpass not installed\n{green}installing...{reset}')
         subprocess.run(["sudo", "apt", "install", "-y", package], check=True)
 except ValueError as e:
-    print (f'{Fore.RED}{Back.WHITE}{package} could not be installed...{Style.RESET_ALL}')
+    print (f'{red}{Back.WHITE}{package} could not be installed...{reset}')
 
+#check if ip is valid
 def ipaddr(ip):
     try:
         if '.' in str(ip) or ':' in str(ip):
             socket.inet_aton(ip)
-            print(f'{Fore.CYAN}IP address set to:{Style.RESET_ALL} {ip}')
-            return
+            print(f'{cyan}IP address set to:{reset} {ip}')
         elif 0 < int(ip) < 1000:
             ip = f'{subnet}.{ip}'
-            print(f'{Fore.CYAN}IP address set:{Style.RESET_ALL} {ip}')
+            print(f'{cyan}IP address set:{reset} {ip}')
             return ip
     except ValueError as e:
-        print(f'{Fore.RED}IP-address \"{ip}\" has been declined{Style.RESET_ALL}')
+        print(f'{red}IP-address \"{ip}\" has been declined{reset}')
     return ''
 
+def trial(setting):
+    print(f'\n{red}Host {setting[1]} authentication could not be verified.\nTesting connection. When prompted for adding the host {setting[1]} to known_host, type \'yes\' and press \'Enter\' in order for this script to work. Only accept when the host is trusted.\nAfter accepting or declining the prompt, wait for the process to end. This process will be terminated in about 5 seconds after reading this message.{reset}\n')
+    command = f'ssh {setting[0]}@{setting[1]}'
+    outputs = subprocess.Popen(args=command,
+                               shell=True,
+                               stderr=subprocess.PIPE,
+                               stdout=subprocess.PIPE,
+                               universal_newlines=True)
+    try: out,err = outputs.communicate(timeout=10)
+    except:
+        outputs.kill()
+        outputs.kill()
+    return
+
+#show help
 def usage(err):
     if err is not None and err != 0:
         print (err)
-    print (f'{Style.RESET_ALL}Usage: {Fore.CYAN}{sys.argv[0]} [-h <help>] [-u <username>] [-i <ip-address>] [-w <timeout in seconds>] [<absolute path>]{Style.RESET_ALL}')
-    
-def send(user,ip,items,w):
-    value = input_user = msg = item_type = ''
-    try: int(w)
-    except: w = 30
-    if w == 1: print(f'{Fore.CYAN}Timeout (wait) set:{Style.RESET_ALL} {w} second')
-    else:  print(f'{Fore.CYAN}Timeout (wait) set:{Style.RESET_ALL} {w} seconds')
-    t = len(items)
-    item_list = []
-    for item in items:
-        if os.path.isdir(item):
-            item_list.append(f'folder: {item}')
-        elif os.path.isfile(item):
-            item_list.append(f'file: {item}')
-    setting = [user,ip]
-    i = 0
-    while setting[0] == '' or setting[1] == '':
+    print (f'{reset}Usage: {cyan}{sys.argv[0]} [-h <help>] [-u <username>] [-i <ip-address>] [-w <timeout in seconds>] [<path(s)>]{reset}')
+
+#send item(s)
+def scp(user,ip,item_list,wait_period):
+    value = input_user = msg = item_type = input_err = password = ''
+    for x in [item for item in item_list if not os.path.isdir(item) and not os.path.isfile(item)]: del item_list[item_list.index(x)]
+    b = c = i = input_user = retries = 0
+    success = handled = []
+    setting = [user.replace(' ',''),ip.replace(' ','')]
+    try: int(wait_period)
+    except: wait_period = 30
+    if wait_period == 1: print(f'{cyan}Timeout (wait) set:{reset} {w} second')
+    else: print(f'{cyan}Timeout (wait) set:{reset} {wait_period} seconds')
+    while '' in setting:
         try:
             if i > 1: i = 0
             if i == 0: value = 'Username'
             elif i == 1: value = f'IP (current subnet: {subnet}....)'
             if setting[i] == '':
-                while setting[i] == '':
-                    if i == 0: setting[i] = input(f'Enter the {value.lower()} of the receiver: ')
-                    else: setting[i] = ipaddr(input(f'Enter the {value} of the receiver: '))
-                    if setting[i] != '': print(f'{Fore.CYAN}{value} set:{Style.RESET_ALL} {setting[i]}')
+                if i == 0: setting[i] = input(f'Enter the {value.lower()} of the receiver: ')
+                else: setting[i] = ipaddr(input(f'Enter the {value} of the receiver: '))
+                if setting[i] != '': print(f'{cyan}{value} set:{reset} {setting[i]}')
             i += 1
-            if t == 0:
-                for a in range(6):
-                    if t == 0: t = input_user = int(input('Enter the number of items: '))
-                    else:
-                        for b in range(t):
-                            if input_user == 'exit': break
-                            while 1:
-                                input_user = input(f'Enter absolute path of item {b + 1}: ')
-                                if input_user == 'exit': break
-                                if input_user == 'show list': print('\n'.join())
-                                if input_user == 'show list': print('\n'.join(f'{str(item_list.index(f)+1)}) {str(f)}' for f in item_list))
-                                if input_user in item_list:
-                                    print(f'{Fore.RED}Path "{input_user}" was already in list{Style.RESET_ALL}')
-                                elif os.path.isdir(input_user):
-                                    item_list.append(f'folder: {input_user}')
-                                    print(f'{Fore.GREEN}Folder "{input_user}" was added to the list{Style.RESET_ALL}')
-                                    break
-                                elif os.path.isfile(input_user):
-                                    item_list.append(f'file: {input_user}')
-                                    print(f'{Fore.GREEN}File "{input_user}" was added to the list{Style.RESET_ALL}')
-                                    break
-                                elif input_user != 'show list':
-                                    print(f'{Fore.RED}Path "{input_user}" doesn\'t exit{Style.RESET_ALL}')
         except (KeyboardInterrupt, Exception) as e:
-            if (type(e).__name__) == 'KeyboardInterrupt' or t == 'exit': 
-                msg = 'User ended the process'
+            if (type(e).__name__) == 'KeyboardInterrupt' or input_user == 'exit':
+                msg = 'User ended the process\n'
                 break
-            continue
-                
-    s = f = k = 0
-    key_check = 'cDHHu7t4JL161*dras*x@ewRGvXj*oLx@$H'
-    if t > 0 and msg == '':
-        password = getpass(f'Enter password for {setting[0]}: ')
-        item_list.insert(0,key_check)
-        stop_trying = True
+    while len(item_list) == 0 and '' not in setting:
+        try:
+            if len(item_list) == 0 and c == 0: input_user = input(f'{input_err}Enter the number of items: {reset}')
+            if input_user == 'exit':
+                msg = 'User ended the process\n'
+                break
+            c = int(input_user) if int(input_user) > 0 else 0
+            while len(item_list) < c:
+                 input_user = input(f'Enter absolute path of item {b + 1}: ')
+                 if input_user == 'exit': break
+                 if input_user == 'show list':
+                     print('\n'.join(f'{str(item_list.index(f)+1)}) {str(f)}' for f in item_list))
+                     continue
+                 if os.path.isdir(input_user): item_type = f'{yellow}folder{reset}'
+                 elif os.path.isfile(input_user): item_type = 'file'
+                 else:
+                     print(f'{red}Path "{input_user}" doesn\'t exit{reset}')
+                     continue
+                 item = input_user
+                 if item in item_list:
+                     print(f'{red}Path "{input_user}" was already in list{reset}')
+                 elif os.path.isdir(input_user) or os.path.isfile(input_user):
+                     item_list.append(item)
+                     if len(item_list) < c: i = '(type "show list" to see the status)'
+                     else: i = ''
+                     print(f'{green}{item_type}: {green}{item_list[-1]} was added to the list {i}{reset}')
+                     b += 1
+
+        except (KeyboardInterrupt, Exception) as e:
+            if (type(e).__name__) == 'KeyboardInterrupt' or input_user == 'exit':
+                msg = 'User ended the process\n'
+                break
+            input_err = f'{red}Invalid literal for int() with base 10: {input_user}\nEnter \'exit\' to stop or try again.\n{reset}'
+            input_user = 0
+            pass
+    if len(item_list) > 0 and msg == '':
         for attempt in range(3):
-            if stop_trying == True and attempt > 0: break
-            stop_trying = True
+            if retries == 0 and attempt > 0: break
+            retries = 0
             for item in item_list:
-                if item[:8] == 'folder: ': item_type = item[:8]
-                elif item[:6] == 'file: ': item_type = item[:6]
-                item = item.replace(item_type,'')
-                if item_type == 'folder: ': item_type = f'{Fore.YELLOW}{item_type}{Style.RESET_ALL}'
+                item_type = f'{yellow}folder{reset}' if os.path.isdir(item) else 'file'
                 try:
-                    if item == key_check: command = f'ssh-keygen -l -F {setting[1]}'
-                    else: command = f'sshpass -p {password} scp -r {item} {setting[0]}@{setting[1]}:'
-                    outs,errs = subprocess.Popen(args=command,shell=True,stderr=subprocess.PIPE,stdout=subprocess.PIPE,universal_newlines=True).communicate(timeout=w)
-                    if errs == '':
-                        if item != key_check:
-                            print(f'{Fore.GREEN}Sent to {setting[0]}\'s root folder:{Style.RESET_ALL} {item_type}{item}')                
-                            s += 1
-                    elif errs:
-                        if 'Permission denied' in errs and attempt < 2:
+                    if password == '': password = getpass(f'Enter password for {setting[0]}: ')
+                    #check known_hosts
+                    #if item == item_list[0]: command = f'ssh-keygen -l -F {setting[1]}'
+                    command = f'sshpass -p {password} scp -o StrictHostKeyChecking=yes -r {item} {setting[0]}@{setting[1]}:'
+                    out,err = subprocess.Popen(args=command,
+                                                 shell=True,
+                                                 stderr=subprocess.PIPE,
+                                                 stdout=subprocess.PIPE,
+                                                 universal_newlines=True).communicate(timeout=wait_period)
+                    if err == '':
+                        print(f'{green}Sent to {setting[0]}\'s root folder:{reset} {item_type}: {item}')
+                        handled.append(item)
+                    elif err:
+                        if 'Permission denied' in err and attempt < 2:
+                            retries = 1
                             password = getpass(f'Permission denied. Enter password for {setting[0]}: ')
-                            errs = ''
-                            stop_trying = False
+                            err = ''
                             break
-                        if item != key_check:
-                            print(f'{Fore.RED}Failed to send:{Style.RESET_ALL} {item_type}{item}\n{errs.strip()}')
-                            f += 1
-                    if item == key_check and outs.strip() == '':
-                        print(f'\n{Fore.RED}Host {setting[1]} authentication could not be verified.\nTesting connection. When prompted for adding the host {setting[1]} to known_host, type \'yes\' and press \'Enter\' in order for this script to work. Only accept when the host is trusted.\nAfter accepting or declining the prompt, wait for the process to end. This process will be terminated in about 5 seconds after reading this message.{Style.RESET_ALL}\n')
-                        command = f'ssh {setting[0]}@{setting[1]}'
-                        output = subprocess.Popen(args=command,shell=True,stderr=subprocess.PIPE,stdout=subprocess.PIPE,universal_newlines=True)
-                        try: outs,errs = output.communicate(timeout=10)
-                        except ValueError as e:
-                            print(e)
-                            pass
-                        if outs == 'Host key verification failed.': f = t
-                        if 'No route to host' in errs:
-                            stop_trying = True
-                            print ('Error message:',errs.replace('\n',''))
-                            print(f'Failed to send:{Fore.RED}')
-                            del item_list[0]
-                            print(f'{Fore.RED}\n'.join(map(str, item_list)))
-                            f = t
+                        if 'Could not resolve hostname' in err or 'Host key verification failed.' in err or 'No route to host' in err:
+                            msg = out if err == '' else err
+                            if 'Could not resolve hostname' in err or 'Host key verification failed.' in err:
+                                msg += f'{cyan}Try "ssh {setting[0]}@{setting[1]}" on the command line first, and type "yes" to accept {setting[1]} to be added to known_hosts{reset}\n'
                             break
-                        output.kill()
-                        print('\nProcess terminated. Continuing scp...')
+                        else:
+                            print(f'{red}Failed to send:{reset} {item_type}: {item}\n{err.strip()}')
+                            handled.append(item)
                         continue
-                except (KeyboardInterrupt, Exception) as e:
+                except (KeyboardInterrupt, Exception,ValueError) as e:
                     if (type(e).__name__) == 'KeyboardInterrupt':
                         msg = 'User ended the process\n'
-                        del item_list[0]
-                        print(f'\nFailed to send:{Fore.RED}')
-                        if os.path.isdir(item): item = f'folder: {item}'
-                        else: item = f'file: {item}'
-                        if item != key_check:
-                            del item_list[0:item_list.index(item)]
-                        print(f'{Fore.RED}\n'.join(map(str, item_list)))
                         break
                     else:
                         e = repr(e).replace(password,'**********')
                         if 'Timeout' in repr(e):
-                            e = f'{Fore.RED}Timeout of {w} seconds exceeded...{Style.RESET_ALL}'
-                        print(f'{Fore.RED}Failed to send:{Style.RESET_ALL} {item_type}{item}:',e)
-                        f += 1
+                            e = f'{red}Timeout of {wait_period} seconds exceeded...{reset}'
+                        elif 'Timeout' not in repr(e):
+                            print(f'{red}Failed to send:{reset} {item_type}: {item}:',e)
                         continue
-    if s == t: msg += f'{Fore.GREEN}Sent: {s}/{t}{Style.RESET_ALL}'
-    if s != t and s > 0: msg += f'{Fore.YELLOW}Sent: {s}/{t}{Style.RESET_ALL}'
-    if f == t or s == 0: msg += f'{Fore.RED}Sent: {s}/{t}{Style.RESET_ALL}'
+    #report
+    if len(handled) != len(item_list):
+        print('Failed to send:')
+        for x in [item for item in item_list if item not in handled]:
+            if os.path.isdir(x): item_type = f'{yellow}folder'
+            else: item_type = 'file'
+            print(f'{item_type}: {red}{x}')
+    s = len(handled)
+    t = len(item_list)
+    f = t - s
+    if s == t and s != 0: msg += f'{green}Sent: {s}/{t}{reset}'
+    if s != t and s > 0: msg += f'{yellow}Sent: {s}/{t}{reset}'
+    if f == t or s == 0: msg += f'{red}Sent: {s}/{t}{reset}'
     print(msg)
 
+#start process
 if __name__ == '__main__':
     user = ip = ''
-    w = 30
+    wait_period = 30
     try:
         (opts, args) = getopt.getopt(sys.argv[1:], 'hu:i:w:', ['user','ip','w'])
         for opt, arg in opts:
-            if opt in ('-h', '--help'): sys.exit(usage('Error occurred: {Fore.RED}{err}'))
-            elif opt in ('-u', '--user'): user = arg 
+            if opt in ('-h', '--help'): sys.exit(usage('Error occurred: {red}{err}'))
+            elif opt in ('-u', '--user'): user = arg
             elif opt in ('-i', '--ip') and '.' in arg: ip = arg
             elif opt in ('-i', '--ip'): ip = int(arg)
-            elif opt in ('-w', '--wait'): w = int(arg)
-        if ip != '': ip = ipaddr(ip)            
+            elif opt in ('-w', '--wait'): wait_period = int(arg)
+        if ip != '': ip = ipaddr(ip)
         items = '$'.join(args)
         if '$' in items: items = items.split('$')
         else: items = [items]
     except (getopt.GetoptError,Exception) as err:
-        sys.exit(usage(f'Error occurred: {Fore.RED}{err}'))
-    send(user,ip,items,w)
+        sys.exit(usage(f'Error occurred: {red}{err}'))
+    scp(user,ip,items,wait_period)
